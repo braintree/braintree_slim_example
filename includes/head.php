a85f468734f0c4ab697bff06b8466edc9542e2ad
lilaconlee <code@getbraintree.com>
2017-06-02 19:47:48 +0000

    Update Drop-in and CSS

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,5 @@
 <head>
+    <meta charset="UTF-8">
     <title>BraintreePHPExample</title>
     <link rel=stylesheet type=text/css href="/static/css/app.css">
     <link rel=stylesheet type=text/css href="/static/css/overrides.css">
