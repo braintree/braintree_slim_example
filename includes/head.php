b9b93acbd4772ca191b42e9a1debad19e883e850
Blade Barringer <blade@crookedneighbor.com>
2017-12-05 08:32:41 -0600

    Update demo page (#10)

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,4 @@
     <title>BraintreePHPExample</title>
     <link rel=stylesheet type=text/css href="/static/css/app.css">
     <link rel=stylesheet type=text/css href="/static/css/overrides.css">
-    <script src="/static/javascript/vendor/jquery-2.1.4.min.js"></script>
-    <script src="/static/javascript/vendor/jquery.lettering-0.6.1.min.js"></script>
-    <script src="/static/javascript/demo.js"></script>
 </head>
