3c81c57332e7dabcdf7c5528db9e371143780ddb
blade <blade.barringer@getbraintree.com>
2017-12-26 20:56:34 -0600

    Use an instantiated gateway object instead of static method calls

--- includes/braintree_init.php
+++ includes/braintree_init.php
@@ -5,8 +5,3 @@ if(file_exists(__DIR__ . "/../.env")) {
     $dotenv = new Dotenv\Dotenv(__DIR__ . "/../");
     $dotenv->load();
 }
-
-Braintree\Configuration::environment(getenv("BT_ENVIRONMENT"));
-Braintree\Configuration::merchantId(getenv("BT_MERCHANT_ID"));
-Braintree\Configuration::publicKey(getenv("BT_PUBLIC_KEY"));
-Braintree\Configuration::privateKey(getenv("BT_PRIVATE_KEY"));
